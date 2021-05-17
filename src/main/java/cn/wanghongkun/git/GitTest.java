70bd69f032dd1718df28420780d12a05531a1e76
whk <1942000810@qq.com>
2021-05-17 10:21:36 +0800

    second commit

--- src/main/java/cn/wanghongkun/git/GitTest.java
+++ src/main/java/cn/wanghongkun/git/GitTest.java
@@ -1,7 +1,9 @@
 package cn.wanghongkun.git;
 
 public class GitTest {
+
     public static void main(String[] args) {
         System.out.println("hello git");
+        System.out.println("hello git2");
     }
 }
